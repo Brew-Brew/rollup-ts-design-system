6b6c829265b216c0e65e0b140e3ced78e2ba10fd
seungkyu2 <zx6658@naver.com>
2020-05-24 20:40:48 +0900

    doc: add readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # rollup-ts-design-system
 
-example 깃헙
+![image](http://localhost:8000/static/829a156f5ab36a74ce0b3e4165ac2854/5ac6b/rolluptsstory.png)
+
+rollup.js + typescript + storybook으로 구축하는 디자인시스템 example repo
--- project/src/App.tsx
+++ project/src/App.tsx
@@ -7,6 +7,8 @@ import Button, {
 function App() {
   return (
     <div>
+      <h5>TS + Rollup.js + storybook 디자인 시스템 사용 예제</h5>
+      <br />
       <Button>버튼</Button>
       <Button theme={ButtonType.PRIMARY}>버튼</Button>
       <Button theme={ButtonType.SECONDARY}>버튼</Button>
